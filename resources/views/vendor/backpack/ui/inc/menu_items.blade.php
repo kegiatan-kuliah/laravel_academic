37f5df907a287a015603ee34141c2e97812362b4
Ari P <ariteknologi@gmail.com>
2025-01-17 23:59:32 +0700

    add teacher crud

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="Students" icon="la la-graduation-cap" :link="backpack_url('student')" />
+<x-backpack::menu-item title="Teachers" icon="la la-school" :link="backpack_url('teacher')" />
